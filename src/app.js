f331867c96f522fbd85f1420b5a14d5f0c889699
kvillescaz <129224791+kvillescaz@users.noreply.github.com>
2023-10-12 15:34:41 -0700

    added search engine

--- src/app.js
+++ src/app.js
@@ -44,9 +44,20 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "ef60ad89788e0edb952855ab3db60998";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-let apiKey = "ef60ad89788e0edb952855ab3db60998";
-let city = "Phoenix";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Phoenix");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
