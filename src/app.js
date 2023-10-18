e7d6398241793e5991118f242e782e5e9eb05689
kvillescaz <129224791+kvillescaz@users.noreply.github.com>
2023-10-18 16:33:12 -0700

    fixed img and added api call

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
@@ -31,7 +31,8 @@ function displayForecast() {
       forecastHTML +
       `<div class="col-2">
                 <div class="weather-forecast-date">${day}</div>
-                <img src="src="http://openweathermap.org/img/wn/50d@2x.png"
+                <img 
+                src="http://openweathermap.org/img/wn/50d@2x.png"
                 alt="" width="42" />
                 <div class="weather-forecast-temperatures">
                   <span class="weather-forecast-temperature-max"> 18° </span>
@@ -43,6 +44,14 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "ef60ad89788e0edb952855ab3db60998";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -67,7 +76,10 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
+
 function search(city) {
   let apiKey = "ef60ad89788e0edb952855ab3db60998";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
