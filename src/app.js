a9aa23e4a950ef262629b53c97a12e4a523b88a8
kvillescaz <129224791+kvillescaz@users.noreply.github.com>
2023-10-18 16:43:50 -0700

    fixed api key

--- src/app.js
+++ src/app.js
@@ -48,7 +48,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "ef60ad89788e0edb952855ab3db60998";
+  let apiKey = "63214c4281922e3bb72fdf12dada7734";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
@@ -81,7 +81,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "ef60ad89788e0edb952855ab3db60998";
+  let apiKey = "63214c4281922e3bb72fdf12dada7734";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
