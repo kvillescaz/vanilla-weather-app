07aee700ed74985386a74ad3df0fc81c86a7302c
kvillescaz <129224791+kvillescaz@users.noreply.github.com>
2023-10-23 16:52:08 -0700

    removed console.log & moved sourcecode

--- src/app.js
+++ src/app.js
@@ -68,7 +68,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "63214c4281922e3bb72fdf12dada7734";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
